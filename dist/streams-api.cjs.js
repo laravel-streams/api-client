9269fa6a38655539304bc6b9421a27e11e4b82ea
Robin Radic <rradic@hotmail.com>
2022-01-04 06:52:57 +0100

    Fixes small request bug

--- dist/streams-api.cjs.js
+++ dist/streams-api.cjs.js
@@ -22798,28 +22798,26 @@ class Request {
     const baseURL = [url.protocol, '//', url.host, '/api'].join('');
     Request.getDefaultConfig = () => ({
         baseURL,
-        request: {
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest',
-            },
-            // `responseType` indicates the type of data that the server will respond with
-            // options are: 'arraybuffer', 'document', 'json', 'text', 'stream'
-            //   browser only: 'blob'
-            responseType: 'json',
-            // `xsrfCookieName` is the name of the cookie to use as a value for xsrf token
-            xsrfCookieName: 'XSRF-TOKEN',
-            // `xsrfHeaderName` is the name of the http header that carries the xsrf token value
-            xsrfHeaderName: 'X-XSRF-TOKEN',
-            withCredentials: false,
-            validateStatus: function (status) {
-                return status >= 200 && status < 300; // default
-            },
-            // `decompress` indicates whether or not the response body should be decompressed
-            // automatically. If set to `true` will also remove the 'content-encoding' header
-            // from the responses objects of all decompressed responses
-            // - Node only (XHR cannot turn off decompression)
-            decompress: true, // default
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest',
         },
+        // `responseType` indicates the type of data that the server will respond with
+        // options are: 'arraybuffer', 'document', 'json', 'text', 'stream'
+        //   browser only: 'blob'
+        responseType: 'json',
+        // `xsrfCookieName` is the name of the cookie to use as a value for xsrf token
+        xsrfCookieName: 'XSRF-TOKEN',
+        // `xsrfHeaderName` is the name of the http header that carries the xsrf token value
+        xsrfHeaderName: 'X-XSRF-TOKEN',
+        withCredentials: false,
+        validateStatus: function (status) {
+            return status >= 200 && status < 300; // default
+        },
+        // `decompress` indicates whether or not the response body should be decompressed
+        // automatically. If set to `true` will also remove the 'content-encoding' header
+        // from the responses objects of all decompressed responses
+        // - Node only (XHR cannot turn off decompression)
+        decompress: true, // default
     });
 })(Request || (Request = {}));
 
